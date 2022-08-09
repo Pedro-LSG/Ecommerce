06284e6ef8db31e146cd351e1f65fd83ea220ea6
pedrogomes_frwk <pedrogomes_frwk@3TH8BH2>
2022-08-09 11:20:30 -0300

    FEAT | Adicionando redirecionamento para HTTPS e configurando resources, scopes e clients.

--- Ecommerce.IdentityServer/Program.cs
+++ Ecommerce.IdentityServer/Program.cs
@@ -1,7 +1,37 @@
+using Ecommerce.IdentityServer.Configuration;
+using Ecommerce.IdentityServer.Model;
+using Ecommerce.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//Adding connection string
+var connection = builder.Configuration["MySqlConnnection:MySqlConnnectionString"];
+builder.Services.AddDbContext<MySqlContext>(options =>
+    options.UseMySql(connection, new MySqlServerVersion(
+        new Version(8, 0, 30))));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySqlContext>()
+    .AddDefaultTokenProviders();
+
+var b = builder.Services.AddIdentityServer(options =>
+    {
+        options.Events.RaiseErrorEvents = true;
+        options.Events.RaiseInformationEvents = true;
+        options.Events.RaiseFailureEvents = true;
+        options.Events.RaiseSuccessEvents = true;
+        options.EmitStaticAudienceClaim = true;
+    }).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+    .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+    .AddInMemoryClients(IdentityConfiguration.Clients)
+    .AddAspNetIdentity<ApplicationUser>();
+
+b.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
@@ -10,9 +40,12 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
